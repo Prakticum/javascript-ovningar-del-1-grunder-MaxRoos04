a5c38ac6f4db688f45efc6b419978416050a2199
MaxRoos04 <maxroos931@gmail.com>
2021-01-13 11:36:47 +0200

    färdig

---
+++ .github/önv1extra/extra.js
@@ -0,0 +1,30 @@
+/**uppgift 1 */
+
+var txt = "kalle tycker om att koda"; 
+
+/** uppgfift 2 */
+console.log(txt.lengtht);
+
+/**uppgfit 3 */
+
+console.log(txt.indexOf("koda"));
+
+/**uppgift 4 */
+
+console.log(txt.slice(txt.indexOf("mycket"), txt.indexOf("mycket")+7));
+
+/** uppgfit 5 */
+
+console.log(txt.replace("mycket", "inte"));
+
+/** uppgift 6 */
+
+console.log(txt.toUpperCase());
+
+/** uppgift 7-9 */
+
+const num1 = 9; 
+console.log(math.pow(num1,2));
+console.log(num1.toFixed(2)); 
+
+
